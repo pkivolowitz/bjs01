0e677e2acb5b90b67e9ce3956b3ff6c6948da9ec
Perry Kivolowitz <perry@kivolowitz.com>
2021-10-31 12:41:29 -0500

    added Tower and abstracted some of the geometry

--- brick.js
+++ brick.js
@@ -3,10 +3,14 @@
 class Brick {
 	static brick_counter = 0;
 	static wood = null;
-	static height = 4;
+	static height = 5;
+	static wd = Brick.height / 3;
 
 	constructor(scene) {
-		this.brick = BABYLON.MeshBuilder.CreateBox('brick' + Brick.brick_counter.toString(), { width: 1, depth: 1, height: Brick.height }, scene);
+		let wd = Brick.wd;
+		let h = Brick.height;
+		let v = wd * wd * h;
+		this.brick = BABYLON.MeshBuilder.CreateBox('brick' + Brick.brick_counter.toString(), { width: wd, depth: wd, height: h }, scene);
 		if (Brick.wood == null) {
 			Brick.wood = new BABYLON.StandardMaterial("wood0", scene);
 			Brick.wood.diffuseTexture = new BABYLON.Texture("./textures/w.jpeg", scene);
@@ -15,7 +19,7 @@ class Brick {
 		this.brick.renderingGroupId = 1;
 		this.brick.material = Brick.wood;
 		this.brick.physicsImposter = new BABYLON.PhysicsImpostor(this.brick, BABYLON.PhysicsImpostor.BoxImpostor, 
-			{ disableBidirectionalTransformation: false, mass: 4, restitution: 0.0, friction: 0.9 }, scene);
+			{ disableBidirectionalTransformation: false, mass: v, restitution: 0.0, friction: 0.9 }, scene);
 		Brick.brick_counter += 1;
 	}
 
@@ -34,5 +38,33 @@ class Brick {
 		this.brick.physicsImposter.forceUpdate()
 		//this.brick.physicsImposter.setLinearVelocity(new BABYLON.Vector3(0, 0, 0));
 		//this.brick.physicsImposter.setAngularVelocity(new BABYLON.Quaternion(0, 0, 0, 1));
+	}
+
+	Rotate(axis, angle) {
+		this.brick.rotate(axis, angle);
+		this.brick.physicsImposter.forceUpdate()
+	}
+}
+
+class Tower {
+	static bricks = [];
+	static levels = 21;
+
+	constructor(scene) {
+		let delta = Brick.wd;
+		let y = Brick.wd / 2;
+		for (var l = 0; l < Tower.levels; l++) {
+			let x = -delta;
+			for (var i = 0; i < 3; i++) {
+				let b = new Brick(scene);
+				b.SetPosition(new BABYLON.Vector3((l % 2) ? x : 0, y, (l % 2) ? 0 : x))
+				b.Rotate(BABYLON.Axis.Z, Math.PI / 2);
+				if (l % 2)
+					b.Rotate(BABYLON.Axis.X, Math.PI / 2);
+				Tower.bricks.push(b);
+				x += delta;
+			}
+			y += delta;
+		}
 	}
 }
